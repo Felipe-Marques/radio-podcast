b1861e5cf0d1d624bc7884eff23715cf8cfb1080
Felipe-Marques <felipe.souzamarques@outlook.com>
2020-05-25 20:29:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+[PT-BR]
+
+# Projeto Radio Podcast - Bootcamp IGTI
+
+Professor: Raphael Gomide.<br>
+Aluno: Felipe Marques.
+
+## :memo: Status:
+
+- Concluído.
+
+## :dart: Objetivo:
+
+Exercitar conceitos trabalhados no Módulo 1 do Bootcamp Dev Full Stack como:
+
+- Declarar elementos HTML como títulos, input, div, span, ul, li etc.<br>
+- Estilizar o app com CSS.<br>
+- Mapear elementos do DOM para serem manipulados com JavaScript.<br>
+- Formatar valores com JavaScript.<br>
+- Realizar diversos cálculos com array methods como map, filter e reduce.<br>
+-
+
+## Pesquisa de Usuários em API
+
+![Pesquisa de Usuários](img/radio-podcast.png)
+
+## :octocat: Projeto:
+
+[Url do projeto](url)
---
+++ js/script.js
@@ -0,0 +1,58 @@
+window.addEventListener('load', start);
+
+function start() {
+  var inputFrequency = document.querySelector('.input-freq');
+
+  inputFrequency.addEventListener('input', handleInputFrequencyChange);
+  showPodcastFromFrequency('87.5');
+}
+
+function handleInputFrequencyChange(event) {
+  var currentFrequency = event.target.value;
+
+  var inputReaders = document.querySelector('.input-reader');
+  inputReaders.value = currentFrequency;
+
+  showPodcastFromFrequency(currentFrequency);
+}
+
+function showPodcastFromFrequency(frequency) {
+  var divPodcasts = document.querySelector('#divPodcasts');
+
+  // ES5
+  /*
+  var hasPodcast = false;
+  var podcast = null;
+
+  for (var i = 0; i < realPodcasts.length; i++) {
+    var currentPodcast = realPodcasts[i];
+
+    if (currentPodcast.id === frequency) {
+      hasPodcast = true;
+      podecast = currentPodcast;
+    }
+  }*/
+
+  //ES6+
+  var currentPodcast = realPodcasts.find((podcast) => {
+    return frequency === podcast.id;
+  });
+
+  if (!!currentPodcast) {
+    renderPodcastData(currentPodcast);
+  } else {
+    divPodcasts.textContent = 'Nenhum Podcast encontrado!';
+  }
+}
+
+function renderPodcastData(podcast) {
+  var { img, title, description } = podcast;
+
+  divPodcasts.innerHTML = `
+  <h2> ${title}</h2>
+  <img src='../img/img/${img}' />
+  <p>${description}</p>
+  `;
+
+  divPodcasts.img.width = '50';
+}
